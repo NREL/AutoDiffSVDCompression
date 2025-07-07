16d8562d241bc83988876992bb4618d1a188baab
Jonathan Maack <jonathan.maack@nrel.gov>
2025-07-07 09:04:19 -0600

    Updates to kestrel running stuff

--- burgers_tests/burgers_sweep.jl
+++ burgers_tests/burgers_sweep.jl
@@ -1,33 +1,50 @@
 
-function sbatch_script(ad::Symbol, target::Symbol, k::Int, run::Int, script::AbstractString)
-    case_str = make_case_string(ad, target, k, run)
+function sbatch_script(
+    ad::Symbol,
+    target::Symbol,
+    k::Int,
+    run::Int,
+    script::AbstractString,
+    optimization::Bool
+)
+    case_str = make_case_string(ad, target, k, run, optimization)
+    mode_arg = optimization ? "optimization" : "gradient"
+    mode_dir = optimization ? "optimization" : "gradient"
     header = """
 #!/bin/bash
 #SBATCH --account=diffprog
 #SBATCH --time=2-0:00:00
 #SBATCH --job-name=bsw_$(case_str)
 #SBATCH --mail-user=jonathan.maack@nrel.gov
-#SBATCH --mail-type=ALL
-#SBATCH --output=bsw_$(case_str).%j.log  # %j will be replaced with the job ID
+#SBATCH --mail-type=NONE
+#SBATCH --output=results/kestrel/$(mode_dir)/bsw_$(case_str).%j.log  # %j will be replaced with the job ID
 """
     body = """
 source \${HOME}/.bash_profile
 module load julia/1.11
 cd /projects/diffprog/jmaack/AutoDiffSVDCompression/burgers_tests
 
-julia $script --case=$(ad) --gridsize=$(k) --seed=$(run) --target=$(target) --trace
+julia $script --case=$(ad) --gridsize=$(k) --seed=$(run) --target=$(target) --trace --$(mode_arg)
 """
     script = header * body
     return script
 end
 
-function make_case_string(ad, target, k, run)
-    return "$(ad)_$(target)_gs$(k)_$(run)"
+function make_case_string(ad, target, k, run, optimization::Bool)
+    mode = optimization ? "opt" : "grad"
+    return "$(ad)_$(target)_$(mode)_gs$(k)_$(run)"
 end
 
-function write_sbatch_script(ad::Symbol, k::Int, seed::Int, tar::Symbol, script::AbstractString)
-    script_str = sbatch_script(ad, tar, k, seed, script)
-    sname = "sbatch_" * make_case_string(ad, tar, k, seed) * ".sh"
+function write_sbatch_script(
+    ad::Symbol,
+    k::Int,
+    seed::Int,
+    tar::Symbol,
+    script::AbstractString;
+    optimization::Bool=false
+)
+    script_str = sbatch_script(ad, tar, k, seed, script, optimization)
+    sname = "sbatch_" * make_case_string(ad, tar, k, seed, optimization) * ".sh"
     io = open(sname, "w")
     write(io, script_str)
     close(io)
@@ -39,13 +56,14 @@ function main()
     RUNNING_KESTREL = Sys.islinux()
     # RUNNING_KESTREL = true
 
+    optimization = false
     ad_modes = [:forward, :reverse, :dirreverse, :impreverse, :svdreverse, :finitediff]
     # ad_modes = [:reverse, :impreverse]
-    grid_sizes = 4:16
+    grid_sizes = 6:8
     # seeds = 1:10
     seeds = 1:1
     # targets = [:sin, :cliff, :weierstrass]
-    targets = [:cliff, :weierstrass]
+    targets = [:sin]
 
     println("Sweeping through AD modes: ", ad_modes)
     println("With grid sizes: ", 2 .^ grid_sizes)
@@ -75,13 +93,17 @@ function main()
                         continue
                     end
                     if RUNNING_KESTREL
-                        sbatch_file = write_sbatch_script(ad, k, seed, tar, burger_script)
+                        sbatch_file = write_sbatch_script(ad, k, seed, tar, burger_script; optimization=optimization)
                         cmd = `sbatch $sbatch_file`
                         println(cmd)
                         run(cmd)
                     else
                         println("Running job: (", ad, ", ", k, ", ", seed, ", ", tar, ")")
-                        cmd = `julia $burger_script --case=$(ad) --gridsize=$(k) --seed=$(seed) --target=$(tar) --trace --gradient`
+                        if optimization
+                            cmd = `julia $burger_script --case=$(ad) --gridsize=$(k) --seed=$(seed) --target=$(tar) --trace --optimization`
+                        else
+                            cmd = `julia $burger_script --case=$(ad) --gridsize=$(k) --seed=$(seed) --target=$(tar) --trace --gradient`
+                        end
                         println(cmd)
                         run(cmd)
                     end
